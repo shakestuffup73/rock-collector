ba8c16fab67cf8e9203dc5ee946f81091e5f66e7
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-20 16:53:58 -0500

    update allowed host

--- rockcollector/settings.py
+++ rockcollector/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rock-collector-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
