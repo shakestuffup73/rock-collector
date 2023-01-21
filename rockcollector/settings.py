cb34295bac25662a4239bb1a2b9e3b10784e7f42
Amanda Stern <amanda.stern.73@gmail.com>
2023-01-21 14:57:36 -0500

    update app

--- rockcollector/settings.py
+++ rockcollector/settings.py
@@ -137,5 +137,5 @@ LOGOUT_REDIRECT_URL = 'home'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
